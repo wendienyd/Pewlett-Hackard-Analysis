ac1424130a17027bed20818b96d1b0f5930d395b
wendienyd <101779483+wendienyd@users.noreply.github.com>
2022-07-27 13:30:30 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,19 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
 
+SELECT DISTINCT ON(emp.emp_no) emp.emp_no,
+	emp.first_name,
+	emp.last_name,
+	emp.birth_date,
+	de.from_date,
+	de.to_date,
+	tl.title
+INTO mentorship_eligibility
+FROM employees AS emp
+INNER JOIN dept_employee AS de
+ON (emp.emp_no = de.emp_no)
+INNER JOIN titles AS tl
+ON (emp.emp_no = tl.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp.emp_no;
